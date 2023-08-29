ab13f5183df19de98e4fc3ec0e8b8a0025e879ce
girishjaiswal1993 <girish.jaiswal@caratred.com>
2023-08-29 22:54:08 +0530

    gatepass js and python

--- gatepass/gatepass/doctype/gate_pass/gate_pass.js
+++ gatepass/gatepass/doctype/gate_pass/gate_pass.js
@@ -2,7 +2,46 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Gate Pass', {
-	// refresh: function(frm) {
+	party: function(frm) {
+		if(frm.doc.party != '' && frm.doc.party_type != ''){
+			frappe.call({
+				method:"gatepass.gatepass.doctype.gate_pass.gate_pass.customer_address_details",
+				args:{
+					party:frm.doc.party,
+					party_type:frm.doc.party_type,
+					company:frm.doc.company
+				},
+				callback: function(r){
+					if (r.message){
+						frm.set_value("company_address_name",r.message.company_address)
+						frm.set_value("shipping_address_name",r.message.shipping_address_name)
+						
+						let company_address_display = r.message.company_address_display.replace("<br>\n","\n")
+						let company_details = company_address_display.split("<br>")
 
-	// }
+						let comp_value = ''
+						for (let i = 0; i < company_details.length; i++) {
+							comp_value += company_details[i] + '\n'
+
+						};
+						frm.set_value("company_address",comp_value);
+
+						let shipping_address = r.message.shipping_address.replace("<br>\n","\n");
+						let new_value = shipping_address.split("<br>");
+
+						let custom_value = '';
+
+						for (let j = 0; j < new_value.length; j++) {
+							custom_value += new_value[j] + '\n'
+
+						};
+						
+						frm.set_value("shipping_address",custom_value);
+					}
+				}
+	
+			})
+		}
+		
+	}
 });
--- gatepass/gatepass/doctype/gate_pass/gate_pass.py
+++ gatepass/gatepass/doctype/gate_pass/gate_pass.py
@@ -1,8 +1,26 @@
 # Copyright (c) 2023, Ganu Reddy and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from erpnext.accounts.party import  get_party_details
 
 class GatePass(Document):
 	pass
+
+
+@frappe.whitelist()
+def customer_address_details(party,party_type,company):
+        
+    party_details = get_party_details(
+        party=party,
+		party_type=party_type,
+		company=company,
+		doctype=None,
+		party_address=None,
+		company_address=None,
+		shipping_address=None,
+	)
+    print(party_details,";;;;;;;;;;;;;;;;;;;;")
+        
+    return party_details
